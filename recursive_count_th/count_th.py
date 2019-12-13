3083d61a094c21f508098228cf779d08318c0911
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-13 11:30:59 -0700

    Planning an psuedocoding count_th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -2,9 +2,29 @@
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
+
+Understanding:
+1. inputs: 1 word = sting
+2. total count th occurs
+3. Case Matters --> th count will only matter if the input includes lowercase. If it is "TH" then we don't add to count
+4. use recursion - no loops
+    recursion is a loop
+5. needs to include if input is none count = 0
+
+Plan:
+
+1. Need to find all lowercase th
+2. check combination of 2 letters along the length of the string
+    isolate every 2 letters in string
+3. for every 2 letter combination that has "th" add to count (but can use for loop)
+    count_th +1???
+
+
 '''
 def count_th(word):
+    print(len(word)) # wreath length ==> 6
     
-    # TBC
-    
-    pass
+    print(word[0:2]) # isolates 2 letters
+
+
+count_th("wreath")
